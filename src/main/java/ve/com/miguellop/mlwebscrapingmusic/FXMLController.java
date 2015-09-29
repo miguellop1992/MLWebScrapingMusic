f827b40742af63250e39e65d9e3691c4a917d46a
Miguel lopez <developer@miguellop.com.ve>
2015-09-28 23:03:26 -0430

    make a reproductor with webview

--- src/main/java/ve/com/miguellop/mlwebscrapingmusic/FXMLController.java
+++ src/main/java/ve/com/miguellop/mlwebscrapingmusic/FXMLController.java
@@ -24,6 +24,7 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
 import javafx.util.Duration;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -34,29 +35,16 @@ import org.jsoup.select.Elements;
 public class FXMLController implements Initializable {
     
     
-    @FXML
-    private MediaView media;
-    
     @FXML 
     private TextField txt;
-    
     @FXML 
     private TableView<Track> table;
-    
     @FXML
-    private Slider timeSlider;
-    
-    @FXML
-    private Slider volumeSlider;
-    @FXML
-    private Label displayTime;
+    private WebView web;
     @FXML
     private Text text;
-    @FXML
-    private ProgressIndicator progress;
     
     private int maxLenght;
-    private Duration duration;
     
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
@@ -66,22 +54,6 @@ public class FXMLController implements Initializable {
     @FXML
     private void handleBackAction(ActionEvent event) throws IOException {
         back();
-    }
-    
-    @FXML
-    private void handlePlayAction(ActionEvent event) throws IOException {
-        if(media.getMediaPlayer()!=null){
-            media.getMediaPlayer().play();
-        }else{
-            playTrack(0);
-        }
-    }
-    
-    @FXML
-    private void handlePauseAction(ActionEvent event) throws IOException {
-        if(media.getMediaPlayer()!=null){
-            media.getMediaPlayer().pause();
-        }
     }
     
     @FXML
@@ -102,21 +74,7 @@ public class FXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         txt.setText("http://www.esenciagaitera.com/gaitas_mp3.htm");
-        media.setFitHeight(200);
-        media.setFitHeight(200);
-        timeSlider.valueProperty().addListener((Observable observable) -> {
-            if(timeSlider.isValueChanging()){
-                media.getMediaPlayer().seek(duration.multiply(timeSlider.getValue()/100d));
-            }
-        });
-        volumeSlider.setValueChanging(true);
-        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if(media.getMediaPlayer()!=null)
-                media.getMediaPlayer().setVolume(newValue.doubleValue());
-            }
-        });
+        
     }    
 
     private void loadWeb(String text) throws IOException {
@@ -129,12 +87,6 @@ public class FXMLController implements Initializable {
             name=l.parent().select("font").first().text();
             author=l.text() ;
             MediaPlayer mp = new MediaPlayer(new Media(url));
-            mp.setOnEndOfMedia(() -> {next();});
-            mp.currentTimeProperty().addListener(o->{updateValue();});
-            mp.setOnReady(()->{ 
-                duration=mp.getMedia().getDuration(); 
-                updateValue();
-            });
             if(name!=null || !name.isEmpty()){
                 table.getItems().add(new Track(name+" - "+author,url,mp));
             }
@@ -146,25 +98,8 @@ public class FXMLController implements Initializable {
     }
     
     private void playTrack(Track track) {
-        if(media.getMediaPlayer()!=null){
-            media.getMediaPlayer().stop();
-            media.setMediaPlayer(null);
-        }
         text.setText(track.getName());
-        media.setMediaPlayer(track.getPlayer());
-        media.getMediaPlayer().setAutoPlay(true);
-        media.getMediaPlayer().play();
-        media.getMediaPlayer().bufferProgressTimeProperty().addListener(new ChangeListener<Duration>() {
-            @Override
-            public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
-                System.out.println("newValue = " + newValue.toSeconds());
-                progress.setProgress(duration.toSeconds()/(newValue.toSeconds()*100));
-            }
-        }
-        );
-        volumeSlider.setValue((int) Math.round(media.getMediaPlayer().getVolume() * 100));
-
-        media.getMediaPlayer().play();
+        web.getEngine().load(track.getUrl());
     }
     
     private void back(){
@@ -186,57 +121,6 @@ public class FXMLController implements Initializable {
     }
     
     
-    protected void updateValue(){
-        if(displayTime!=null && timeSlider != null && volumeSlider != null && media.getMediaPlayer()!=null){
-            Platform.runLater(() -> {
-                Duration currentTime = media.getMediaPlayer().getCurrentTime();
-                displayTime.setText(formatTime(currentTime,duration));
-                timeSlider.setDisable(duration.isUnknown());
-                if (!timeSlider.isDisabled() && duration.greaterThan(Duration.ZERO) && !timeSlider.isValueChanging()) {
-                    timeSlider.setValue(currentTime.divide(duration.toMillis()).toMillis()*100d);
-                }
-                
-            });
-        }
-    }
     
-    private static String formatTime(Duration elapsed, Duration duration) {
-        int intElapsed = (int) Math.floor(elapsed.toSeconds());
-        int elapsedHours = intElapsed / (60 * 60);
-        if (elapsedHours > 0) {
-            intElapsed -= elapsedHours * 60 * 60;
-        }
-        int elapsedMinutes = intElapsed / 60;
-        int elapsedSeconds = intElapsed - elapsedHours * 60 * 60
-                - elapsedMinutes * 60;
-
-        if (duration.greaterThan(Duration.ZERO)) {
-            int intDuration = (int) Math.floor(duration.toSeconds());
-            int durationHours = intDuration / (60 * 60);
-            if (durationHours > 0) {
-                intDuration -= durationHours * 60 * 60;
-            }
-            int durationMinutes = intDuration / 60;
-            int durationSeconds = intDuration - durationHours * 60 * 60
-                    - durationMinutes * 60;
-            if (durationHours > 0) {
-                return String.format("%d:%02d:%02d/%d:%02d:%02d",
-                        elapsedHours, elapsedMinutes, elapsedSeconds,
-                        durationHours, durationMinutes, durationSeconds);
-            } else {
-                return String.format("%02d:%02d/%02d:%02d",
-                        elapsedMinutes, elapsedSeconds, durationMinutes,
-                        durationSeconds);
-            }
-        } else {
-            if (elapsedHours > 0) {
-                return String.format("%d:%02d:%02d", elapsedHours,
-                        elapsedMinutes, elapsedSeconds);
-            } else {
-                return String.format("%02d:%02d", elapsedMinutes,
-                        elapsedSeconds);
-            }
-        }
-    }
     
 }
